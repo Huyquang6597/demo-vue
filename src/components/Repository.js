b51b584a2d4a3b3100c923bc3f7654c1b9fc8055
huytq <huytq@etc.vn>
2022-10-05 15:11:02 +0700

    sua edit form

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -6,6 +6,8 @@ const baseUrl = 'http://localhost:8051/swagger-resources/smovie';
 export default {
     data() {
         return {
+            showModal:false,
+            isEdit:false,
             movies: []
             , movie: {
                 id: '',
@@ -41,7 +43,7 @@ export default {
             valueEdit: {
                 id: '',
                 name: '',
-                type: '',
+                typee: '',
                 price: '',
                 time: '',
                 premiereDate: '',
@@ -52,10 +54,11 @@ export default {
                 director: '',
                 actor: '',
             },
+
             valueAdd: {
                 id: '',
                 name: '',
-                type: '',
+                typee: '',
                 price: '',
                 time: '',
                 premiereDate: '',
@@ -66,6 +69,7 @@ export default {
                 director: '',
                 actor: '',
             },
+
             valueSearchs: {
                 id: '',
                 name: '',
@@ -80,6 +84,7 @@ export default {
                 director: '',
                 actor: '',
             },
+
             page: {
                 page: 1,
                 size: 5,
@@ -101,18 +106,7 @@ export default {
                 })
         },
 
-        // findMoviesByAny() {
-        //     axios.post(`${baseUrl}/find-any`, this.valueSearchs)
-        //         .then(response => {
-        //             console.log(response)
-        //             this.movies = response.data.result;
-        //
-        //
-        //         })
-        //         .catch(error => {
-        //             console.log(error);
-        //         })
-        // },
+
         findMoviesByAny2() {
             axios.post(`${baseUrl}/search`, this.valueSearchs)
                 .then(response => {
@@ -173,9 +167,23 @@ export default {
         },
 
         updateMovie() {
-            axios.put(`${baseUrl}/update`, this.valueEdit)
+            axios.put(`${baseUrl}/update`, {
+                id: this.valueAdd.id,
+                name: this.valueAdd.name,
+                type: this.valueAdd.type,
+                price: this.valueAdd.price,
+                time: this.valueAdd.time,
+                premiereDate: this.valueAdd.premiereDate,
+                description: this.valueAdd.description,
+                statuss: this.valueAdd.statuss,
+                tickets: this.valueAdd.tickets,
+                production: this.valueAdd.production,
+                director: this.valueAdd.director,
+                actor: this.valueAdd.actor,
+            })
                 .then(response => {
                     console.log(response)
+                    this.showMessage('success', 'Update!', 'Chỉnh sửa thành công');
                     this.getAll();
                 })
                 .catch(error => {
@@ -184,8 +192,23 @@ export default {
         },
 
         addMovie() {
+            console.log(this.valueAdd)
+            this.showModal = true;
+            axios.post(`${baseUrl}/save`, {
+                id: this.valueAdd.id,
+                name: this.valueAdd.name,
+                type: this.valueAdd.typee,
+                price: this.valueAdd.price,
+                time: this.valueAdd.time,
+                premiereDate: this.valueAdd.premiereDate,
+                description: this.valueAdd.description,
+                statuss: this.valueAdd.statuss,
+                tickets: this.valueAdd.tickets,
+                production: this.valueAdd.production,
+                director: this.valueAdd.director,
+                actor: this.valueAdd.actor,
 
-            axios.post(`${baseUrl}/save`, this.valueAdd)
+            })
                 .then(response => {
                     console.log(response)
                     console.log(this.valueAdd)
@@ -299,6 +322,37 @@ export default {
 
         resetForm() {
             this.valueAdd = {
+                id:'',
+                name:'',
+                type:'',
+                price:'',
+                time:'',
+                premiereDate:'',
+                description:'',
+                statuss:'',
+                tickets:'',
+                production:'',
+                director:'',
+                actor:'',
+            }
+            this.errors = {
+                id:'',
+                name:'',
+                type:'',
+                price:'',
+                time:'',
+                premiereDate:'',
+                description:'',
+                statuss:'',
+                tickets:'',
+                production:'',
+                director:'',
+                actor:'',
+            }
+        },
+
+        resetFormEdit() {
+            this.valueEdit = {
                 id:'',
                 name:'',
                 type:'',
