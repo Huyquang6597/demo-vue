b74e34e9d7c438a87af3d3c79731248acfc753d8
huytq <huytq@etc.vn>
2022-10-05 10:36:40 +0700

    hien thi thong bao khi xoa

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import moment from "moment";
 
 const baseUrl = 'http://localhost:8051/swagger-resources/smovie';
 
@@ -63,7 +64,7 @@ export default {
                 director: '',
                 actor: '',
             },
-            page:{
+            page: {
                 page: 1,
                 size: 5,
             }
@@ -116,47 +117,203 @@ export default {
             }
         },
 
-    deleteMovie(id) {
-        axios.delete(`${baseUrl}/delete?id=${id}`)
-            .then(
-                this.getAll()
-            )
-            .catch(error => {
-                console.log(error);
+        deleteMovie(id) {
+            this.$swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then ((movies) => {
+                if (movies.value) {
+                    axios.delete(`${baseUrl}/delete?id=${id}`)
+                        .then(response => {
+                            console.log(response)
+                            this.getAll();
+                            this.$swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        })
+                        // .catch(error => {
+                        //     console.log(error);
+                        // })
+                }
             })
-    },
-    findMoviesById(id) {
-        axios.get(`${baseUrl}/search-by-id?id=${id}`)
-            .then(response => {
-                this.valueEdit = response.data[0];
+        },
 
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    },
-    updateMovie() {
-        axios.put(`${baseUrl}/update`, this.valueEdit)
-            .then(response => {
-                console.log(response)
-                this.getAll();
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    },
-    addMovie() {
-        axios.post(`${baseUrl}/save`, this.valueAdd)
-            .then(response => {
-                console.log(response)
-                console.log(this.valueAdd)
-                this.getAll();
-            })
-            .catch(error => {
-                // console.log(this.valueAdd)
-                console.log(error);
-            })
-    },
-}
+        findMoviesById(id) {
+            axios.get(`${baseUrl}/search-by-id?id=${id}`)
+                .then(response => {
+                    this.valueEdit = response.data[0];
+
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        updateMovie() {
+            axios.put(`${baseUrl}/update`, this.valueEdit)
+                .then(response => {
+                    console.log(response)
+                    this.getAll();
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+
+        addMovie() {
+            axios.post(`${baseUrl}/save`, this.valueAdd)
+                .then(response => {
+                    console.log(response)
+                    console.log(this.valueAdd)
+                    this.getAll();
+                })
+                .catch(error => {
+                    // console.log(this.valueAdd)
+                    console.log(error);
+                })
+        },
+
+        validate_name() {
+            this.errors.name = ''
+            if (!this.movies.name) {
+                this.errors.name = 'Tên phim không được để trống'
+                return false
+            }
+            return true
+        },
+
+        validate_price() {
+            this.errors.price = ''
+            if (!this.movies.price) {
+                this.errors.price = 'Giá vé xem phim không được để trống'
+                return false
+            }
+            if (this.movies.price < 0) {
+                this.errors.price = 'Giá vé xem phim không được âm'
+                return false
+            }
+            return true
+        },
+
+        validate_description() {
+            this.errors.description = ''
+            if (!this.movies.description) {
+                this.errors.description = 'Mô tả phim không được để trống'
+                return false
+            }
+            return true
+        },
+
+        validate_type() {
+            this.errors.typee = ''
+            if (!this.movies.typee) {
+                this.errors.typee = 'Thể loại phim không được để trống'
+                return false
+            }
+        },
+
+        validate_production() {
+            this.errors.production = ''
+            if (!this.movies.production) {
+                this.errors.production = 'Hãng phim không được để trống'
+                return false
+            }
+        },
+
+        validate_director() {
+            this.errors.director = ''
+            if (!this.movies.director) {
+                this.errors.director = 'Giám đốc sản xuất phim không được để trống'
+                return false
+            }
+        },
+
+        validate_actor() {
+            this.errors.actor = ''
+            if (!this.movies.actor) {
+                this.errors.actor = 'Diễn viên không được để trống'
+                return false
+            }
+        },
+
+        validate() {
+            if (!this.validate_name()
+                || !this.validate_price()
+                || !this.validate_description()
+                || !this.validate_type()
+                || !this.validate_production()
+                || !this.validate_director()
+                || !this.validate_actor()
+            )
+                return false
+            return true
+        },
+
+        getDate: function (premiereDate) {
+            return moment(premiereDate, 'YYYY-MM-DD').format('DD/MM/YYYY');
+        },
+
+        resetSearch() {
+            this.key = {
+                name: '',
+                typee: '',
+                description: '',
+                production: '',
+                director: '',
+                actor: '',
+            }
+            this.findMoviesByAny2()
+        },
+
+        resetForm() {
+            this.movies = {
+                id: '',
+                name: '',
+                type: '',
+                price: '',
+                time: '',
+                premiereDate: '',
+                description: '',
+                statuss: '',
+                tickets: '',
+                production: '',
+                director: '',
+                actor: '',
+            }
+            this.errors = {
+                id: '',
+                name: '',
+                type: '',
+                price: '',
+                time: '',
+                premiereDate: '',
+                description: '',
+                statuss: '',
+                tickets: '',
+                production: '',
+                director: '',
+                actor: '',
+            }
+        },
+
+        showMessage(icon, title, text) {
+            this.$swal.fire({
+                showConfirmButton: false,
+                timer: 2000,
+                icon: icon,
+                title: title,
+                text: text
+            });
+
+        }
+    }
+
 }
 
